66527c351962e327ea4dae2ac9226645c32582b6
multileo-ai <kumar.ashutosh.er@gmail.com>
2025-10-05 15:32:49 +0530

    bug fixing 1

--- src/pages/ProductTemp.jsx
+++ src/pages/ProductTemp.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import { useNavigate } from "react-router-dom";
 
 const ProductTemp = () => {
